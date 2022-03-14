d646f8c8b841e5b7adc2cc04668c47d947fb21e9
Пеливан Эдуард Владимирович <pelivan96e@gmail.com>
2022-03-15 00:36:17 +0600

    fixed

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('basicAuth')->prefix('v1')->namespace('API\v1')->group(functio
     Route::get('category', 'CategoryController@categories');
     Route::get('search', 'SearchController@search');
     Route::get('user/get-garage/{vin}', 'UserController@getGarage');
+    Route::get('user/category', 'CategoryController@categories');
 });
 
 Route::middleware('auth:sanctum')->prefix('v1')->namespace('API\v1')->group(function (): void {
