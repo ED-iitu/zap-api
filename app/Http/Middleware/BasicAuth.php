dab75f7ca44e07c5b966d454b89338d24563deca
Пеливан Эдуард Владимирович <pelivan96e@gmail.com>
2022-03-15 01:40:12 +0600

    fixed

--- app/Http/Middleware/BasicAuth.php
+++ app/Http/Middleware/BasicAuth.php
@@ -35,26 +35,26 @@ class BasicAuth
      */
     public function handle(Request $request, Closure $next)
     {
-//        if ($this->auth->guard('sanctum')->check()) {
-//            $this->auth->shouldUse('sanctum');
-//        } else {
-//            $AUTH_USER = env('API_USERNAME');
-//            $AUTH_PASS = env('API_PASSWORD');
-//
-//            header('Cache-Control: no-cache, must-revalidate, max-age=0');
-//
-//            $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
-//
-//            $is_not_authenticated = (
-//                !$has_supplied_credentials || $_SERVER['PHP_AUTH_USER'] != $AUTH_USER || $_SERVER['PHP_AUTH_PW'] != $AUTH_PASS
-//            );
-//
-//            if ($is_not_authenticated) {
-//                header('HTTP/1.1 401 Authorization Required');
-//                header('WWW-Authenticate: Basic realm="Access denied"');
-//                exit;
-//            }
-//        }
+        if ($this->auth->guard('sanctum')->check()) {
+            $this->auth->shouldUse('sanctum');
+        } else {
+            $AUTH_USER = env('API_USERNAME');
+            $AUTH_PASS = env('API_PASSWORD');
+
+            header('Cache-Control: no-cache, must-revalidate, max-age=0');
+
+            $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+
+            $is_not_authenticated = (
+                !$has_supplied_credentials || $_SERVER['PHP_AUTH_USER'] != $AUTH_USER || $_SERVER['PHP_AUTH_PW'] != $AUTH_PASS
+            );
+
+            if ($is_not_authenticated) {
+                header('HTTP/1.1 401 Authorization Required');
+                header('WWW-Authenticate: Basic realm="Access denied"');
+                exit;
+            }
+        }
 
         return $next($request);
     }
