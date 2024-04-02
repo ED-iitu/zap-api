f3a06b661e758a48d50077e74b3ff165679118f9
pelivan <pelivan@kolesa.kz>
2024-04-02 19:49:27 +0500

    seeder added

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('roles')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'display_name' => 'admin',
+        ]);
+
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'role_id' => 1,
+            'email' => "admin@admin.com",
+            'password' => Hash::make('password'),
+            'phone' => '87717777777',
+        ]);
+    }
+}
