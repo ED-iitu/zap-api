c8378d4e4956c12fca87104791b1a7e6b70764a9
pelivan <pelivan@kolesa.kz>
2024-04-09 01:18:42 +0500

    log added

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -49,11 +49,11 @@ class ProductImport implements ToModel, WithHeadingRow, WithChunkReading, WithBa
 
     public function chunkSize(): int
     {
-        return 500;
+        return 5;
     }
 
     public function batchSize(): int
     {
-        return 500;
+        return 5;
     }
 }
