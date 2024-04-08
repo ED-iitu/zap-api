8381535e6adefb826bd7f54f7b3623583cb5635c
pelivan <pelivan@kolesa.kz>
2024-04-09 01:13:57 +0500

    log added

--- config/queue.php
+++ config/queue.php
@@ -38,7 +38,9 @@ return [
             'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
-            'retry_after' => 90,
+            'retry_after' => 505,
+            'after_commit' => false,
+            'timeout' => 500,
         ],
 
         'beanstalkd' => [
